4611ffd58710a5bea144448076c20e720f914289
SSOTOCANO <ssotocano@gmail.com>
2020-01-11 16:24:37 -0500

    Connect the project with PostgreSQL database

--- tiendaOnline/tiendaOnline/settings.py
+++ tiendaOnline/tiendaOnline/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tiendaOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_articulosclientes',
+        'USER': 'postgres',
+        'PASSWORD': 'Apolo0210*',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
